69739419c44f579efb83056e7c87450c2610b0c4
Capsonchik <Serega00790@mail.ru>
2022-08-12 14:34:31 +0300

    first commit

---
+++ src/components/discover/discover.jsx
@@ -0,0 +1,14 @@
+import {StyledDiscover} from "./discover.styled";
+import {Container} from "../globalStyled/styled.global";
+
+const Discover = () => {
+  return(
+      <StyledDiscover>
+        <Container>
+          test
+        </Container>
+      </StyledDiscover>
+  )
+}
+
+export default Discover;
